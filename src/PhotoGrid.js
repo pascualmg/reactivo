715feca99b6162ebab45ac6acf935ada93ceb060
Pascual <pascual.munoz@pccomponentes.com>
2018-04-04 20:42:26 +0200

    metiendole un poco la puntica con RX (beta)

--- src/PhotoGrid.js
+++ src/PhotoGrid.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Photo from "./Photo";
 
+import { Observable, Subject, ReplaySubject, from, of, range } from 'rxjs';
+import { map, filter, switchMap } from 'rxjs/operators';
 
 export default class PhotoGrid extends React.Component{
-
-
   iteraConUnMap = function iteraConUnMap()
   {
     const arr = ['algo','algo','algo','algo','algo','algo','algo','algo','algo','algo'];
