1e607a6d92c5362e87c781f93bc0fb097a2d4069
Pascual <pascual.munoz@pccomponentes.com>
2018-04-05 00:30:59 +0200

    omg wtf

--- src/PhotoGrid.js
+++ src/PhotoGrid.js
@@ -4,21 +4,22 @@ import Photo from "./Photo";
 import {Observable, Subject, ReplaySubject, from, of, range} from 'rxjs';
 import {map, filter, switchMap} from 'rxjs/operators';
 
-
 export default class PhotoGrid extends React.Component {
   arr = ['algo', 'algo', 'algo', 'algo', 'algo', 'algo', 'algo', 'algo', 'algo', 'algo'];
 
   iteraConUnObservable = function iteraConUnObservable() {
-    const arr = ['uno', 'do', 'tre', 'cuatro', 'cinco', 'sei', 'siete', 'ocho', 'nueve', 'die'];
+    const flujete$ = this.createObservableFromArray(this.arr);
 
-    const flujete$ = this.createObservableFromArray(arr).subscribe({
-      next: x => {return <span>{x.item}</span>} ,
+    const newArr = [];
+    flujete$
+      .subscribe({
+      next: x => {
+        newArr.push(<span><Photo name={x.item}/></span>)
+      },
       error: err => console.error('something wrong occurred: ' + err),
       complete: () => console.log('done'),
     });
-    return arr.map(
-      (item) => <span><Photo name={item}/></span>
-      );
+    return newArr;
   };
 
   /**
@@ -26,15 +27,21 @@ export default class PhotoGrid extends React.Component {
    * @param arr | El array a observar.
    * @returns {Observable}
    */
-  createObservableFromArray = function createObservableFromArray(arr){
-      return Observable.create(function (observer) {
-          arr.map(
-            (item, i) => observer.next({item: item, i: i})
-          );
-          observer.complete();
-        }
-      );
-    };
+  createObservableFromArray = function createObservableFromArray(arr) {
+    return Observable.create(function (observer) {
+        arr.map(
+          (item, i) => observer.next({item: item, i: i})
+        );
+        observer.complete();
+      }
+    );
+  };
+  iteraConUnMap = function iteraconUnMap() {
+    const arr = ['uno', 'do', 'tre', 'cuatro', 'cinco', 'sei', 'siete', 'ocho', 'nueve', 'die'];
+    return arr.map(
+      (item) => <span><Photo name={item}/></span>
+    );
+  };
 
   render = () => this.iteraConUnObservable();
 }
