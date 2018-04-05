0ef899d86fe8960edf04d06de44baba6ffbad56f
Pascual Muñoz <pascual.munoz@pccomponentes.com>
2018-04-05 10:50:12 +0200

    async await try

--- src/PhotoGrid.js
+++ src/PhotoGrid.js
@@ -4,20 +4,42 @@ import Photo from "./Photo";
 import {Observable} from 'rxjs';
 
 export default class PhotoGrid extends React.Component {
-   arr = ['uno', 'do', 'tre', 'cuatro', 'cinco', 'sei', 'siete', 'ocho', 'nueve', 'die'];
+  arr = ['uno', 'do', 'tre', 'cuatro', 'cinco', 'sei', 'siete', 'ocho', 'nueve', 'die'];
+  state = {};
+  props = {};
+
+  API_URL = 'https://gist.githubusercontent.com/kutyel/8f2d287ceb0a04bf3edb2e435b07f61d/raw';
+
+  async componentWillMount() {
+    try {
+      console.log('antes de montar el componente.',);//TODO: borrame.
+      const res = await fetch(this.API_URL);
+      const posts = await res.json();
+      this.setState({posts});
+      console.log('this.state', this.state);//TODO: borrame.
+      
+    } catch (e) {
+      console.log('error', e);//TODO: borrame.
+    }
+  };
+
+  componentDidMount() {
+
+  };
+
   iteraConUnObservable = function iteraConUnObservable() {
     const flujete$ = this.createObservableFromArray(this.arr);
 
     const newArr = [];
     flujete$
       .subscribe({
-      next: x => {
-        console.log('llega', x);
-        newArr.push(<span><Photo name={x.item}/>{x.name}</span>)
-      },
-      error: err => console.error('something wrong occurred: ' + err),
-      complete: () => console.log('done'),
-    });
+        next: x => {
+          console.log('llega', x);
+          newArr.push(<span><Photo name={x.item}/>{x.name}</span>)
+        },
+        error: err => console.error('something wrong occurred: ' + err),
+        complete: () => console.log('done'),
+      });
     return newArr;
   };
 
