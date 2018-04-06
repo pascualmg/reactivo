6061f271246310979b252ad4f979a813864d8e2d
Pascual Muñoz <pascual.munoz@pccomponentes.com>
2018-04-06 08:18:38 +0200

    async await try

--- src/App.test.js
+++ src/App.test.js
@@ -3,14 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
-  console.log('we');//TODO: borrame.
-
-    document.mierdaconpasta = 'ahora si me defino';
-    const booleAno = document.hasOwnProperty.call(document, 'mierdaconpasta');
-
-    console.log('tengo mierda con pasta', booleAno);//TODO: borrame.
-
-
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
