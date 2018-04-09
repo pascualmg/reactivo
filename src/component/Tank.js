89d33b4f229cdaf8a4ecef282f6bcb5b5470afdb
Pascual <pascual.munoz@pccomponentes.com>
2018-04-09 21:37:47 +0200

    functionalTank done.

--- src/component/Tank.js
+++ src/component/Tank.js
@@ -21,7 +21,7 @@ export default class Tank extends React.Component {
       </progress>;
 
     const Tank =
-      <div>
+      <div className={"Tank"}>
         {bar}
         {info}
       </div>;
--- src/index.js
+++ src/index.js
@@ -5,6 +5,33 @@ import App from './App';
 import Tank from './component/Tank';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<Tank />, document.getElementById('tank'));
+ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<Tank/>, document.getElementById('tank'));
+
+
+function FunctionalTank(props) {
+  console.log('pués me estoy ejecutando macho...',props );//TODO: borrame.
+  const info =
+    <div className="tankInfo">
+      El tanke tiene:{props.material}
+      le cogen : {props.maxCapacity}
+      Contenido actual: {props.actualQuantity}
+      Está ahora mismo al : {(props.actualQuantity / props.maxCapacity) * 100} %
+    </div>;
+
+  const bar =
+    <progress className={"tankBar"} max={props.maxCapacity} value={props.actualQuantity}>
+    </progress>;
+
+  const Tank =
+    <div className={"Tank"}>
+      {bar}
+      {info}
+    </div>;
+
+  return Tank;
+};
+const FunctionalTankElement = <FunctionalTank material="aguica"/>;
+ReactDOM.render(FunctionalTankElement , document.getElementById('functionalTank'));
+
 registerServiceWorker();
